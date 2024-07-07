0debef37fa3e47c6849782b411e4501858afec5d
Manuel Selch <manuel@selch.de>
2024-07-07 17:43:06 +0200

    useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 const useLocalStorage = (storageKey, fallbackState) => {
-  const [value, setValue] = React.useState(
+  const [value, setValue] = useState(
     JSON.parse(localStorage.getItem(storageKey)) ?? fallbackState
   );
 
-  React.useEffect(() => {
+  useEffect(() => {
     localStorage.setItem(storageKey, JSON.stringify(value));
   }, [value, storageKey]);
 
