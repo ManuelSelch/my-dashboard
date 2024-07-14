fce7f9fdee144278056276199505980e0774a151
Manuel Selch <manuel@selch.de>
2024-07-14 02:49:42 +0200

    cors all

--- backend/server.js
+++ backend/server.js
@@ -34,29 +34,21 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+/*
 app.use(cors({
   origin: 'https://dashboard.dev.manuelselch.de'
 }));
+*/
 
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://dashboard.dev.manuelselch.de');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
   next();
 });
+
 app.use(bodyParser.json());
 
 
